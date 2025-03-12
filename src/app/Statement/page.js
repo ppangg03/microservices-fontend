de899525d6ea504b58718696733d59e97d5bf7de
Pang jirawan <jirawan.jit@ku.th>
2025-03-12 15:14:48 +0700

    Update page.js

--- src/app/Statement/page.js
+++ src/app/Statement/page.js
@@ -185,7 +185,7 @@ if(!search) {
             <div className="flex justify-between font-medium mb-1">
 
               <span className="text-sm">วัน-เวลา</span>
-              <div className="flex flex-grow items-center gap-1">
+              <div className="flex items-center gap-1">
               <span className="text-sm">เงินเข้า/เงินออก </span>
               <div className="relative w-1/4">
     
