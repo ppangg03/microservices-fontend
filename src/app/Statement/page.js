ce18fac101c821747037a809a83c8acb86b12329
ppangg03 <jirawan.jit@ku.th>
2025-03-12 13:50:29 +0700

    Update project to latest version

--- src/app/Statement/page.js
+++ src/app/Statement/page.js
@@ -185,7 +185,7 @@ if(!search) {
             <div className="flex justify-between font-medium mb-1">
 
               <span className="text-sm">วัน-เวลา</span>
-              <div className="flex items-center gap-4">
+              <div className="flex flex-grow justify-end items-center gap-2">
               <span className="text-sm">เงินเข้า/เงินออก </span>
               <div className="relative w-1/4">
     
